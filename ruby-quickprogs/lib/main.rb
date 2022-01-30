f757d8c1aca40adeb2ca0d96c695fef436793975
HamnYce <hamadboa@gmail.com>
2022-01-30 11:29:00 +0300

    update files

--- ruby-quickprogs/lib/main.rb
+++ ruby-quickprogs/lib/main.rb
@@ -4,8 +4,14 @@ def palindrome?(num)
   num.digits == num.digits.reverse
 end
 
-def pal(x, y, total)
-  return x * y if palindrome?(x * y)
+def pal(x, y)
+  puts "x:#{x} y:#{y}" or return x * y if palindrome?(x * y)
+
+  if y < 100
+    pal(x - 1, 999)
+  else
+    pal(x, y - 1)
+  end
 end
 
-puts pal(1000, 1000, 1_000_000)
+puts pal(999, 999)
