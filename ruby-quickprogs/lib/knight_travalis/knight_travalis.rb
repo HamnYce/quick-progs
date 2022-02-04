7d218e9e3b0d540d7a756d45ee7748a1be8260e3
HamnYce <hamadboa@gmail.com>
2022-02-04 13:17:56 +0300

    add formula for converting, coordinate to integer position and back

--- ruby-quickprogs/lib/knight_travalis/knight_travalis.rb
+++ ruby-quickprogs/lib/knight_travalis/knight_travalis.rb
@@ -2,7 +2,7 @@
 
 adjacency_matrix = Array.new(64) do |x|
   Array.new(64) do |y|
-    "#{x}, #{y}"
+    true
   end
 end
 
@@ -11,4 +11,15 @@ p adjacency_matrix[0]
 # TODO: construct adjacency matrix with proper connections between squares
 #   that knight can move to and fro.
 
+# we are representing the squares of the board as integers from 0 - 63
+# 0 being 0, 0 (top left corner) and 63 being 7,7 (bottom right corner)
+# to go from coordinate ([x][y] indexes) -> integer postion we do
+# 8*y + x
+# to go from integer position -> coordinate we do
+# x = integer % 8
+# y = integer / 8
+
 def knight_travels(pos1, pos2); end
+
+# Breadth first search of graph and return the moment you reach
+# the required destination (pos2 in knight_travels method)
